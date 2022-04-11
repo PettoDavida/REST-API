d4e43e0e0b47c61e298df37fa0820a62468ce978
Maximilian Widman <42762656+PettoDavida@users.noreply.github.com>
2022-04-11 10:45:43 +0200

    Added ability to add book to database

--- index.js
+++ index.js
@@ -5,20 +5,37 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-let books = [];
+let books = [
+    {
+        "isbn": 9781449331818,
+        "title": "Learning JavaScript Design Patterns",
+        "author": "Addy Osmani",
+        "publish_date": "2012-07-01",
+        "publisher": "O'Reilly Media",
+        "numOfPages": 254
+    },
+    {
+        "isbn": 9781449365035,
+        "title": "Speaking JavaScript",
+        "author": "Axel Rauschmayer",
+        "publish_date": "2014-02-01",
+        "publisher": "O'Reilly Media",
+        "numOfPages": 460
+    }
+];
 
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 
-app.get('/book', (req, res) => {
-    const book = req.body;
+app.post('/book', (req, res) => {
+    const book = req.body
 
-    console.log(book);
     books.push(book)
+    console.log(books);
 
     res.send('Book is added to the database')
 })
 
-app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
+app.listen(port, 'localhost', () => console.log(`Hello world app listening on port ${port}!`))
